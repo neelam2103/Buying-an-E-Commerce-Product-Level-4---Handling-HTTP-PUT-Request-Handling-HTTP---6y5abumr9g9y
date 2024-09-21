6320a4dc09c31f209e4f09657ae35f5e7a84010a
neelam2103 <kneelam849@gmail.com>
2024-09-21 21:46:03 +0530

    code

--- src/app.js
+++ src/app.js
@@ -15,6 +15,40 @@ app.use(express.json());
 // Write PATCH endpoint to buy a product for the client here
 // Endpoint /api/v1/products/:id
 
+app.patch("/api/v1/products/:id",(req,res)=>{
+    
+    const id = Number(req.params.id);
+
+    const product = products.find((obj)=>{
+        return obj.id == id;
+    })
+
+    if(!product){
+     return res.status(404).json({
+         status:"failed",
+         message:"Product not found!"
+     })
+    }
+    else{
+     product.quantity-=1;
+     if(product.quantity>0){
+         return res.status(200).json({
+              status:"success",
+              message:`Thank you for purchasing ${product.name}`,
+              data:{
+                 product
+              }
+         })
+     }
+     else{
+         return res.status(404).json({
+             status:"success",
+             message:`${product.name} , Out of stock!`
+         }) 
+     }
+
+    }
+})
 
 
 
